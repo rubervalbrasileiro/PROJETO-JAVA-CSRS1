b22286c49465ea3ff2f7e31b3fd0a70cd46be4ef
Ruberval Brasileiro <tecdsis40@gmail.com>
2024-10-28 13:54:39 -0300

    Implementação classe usuários em beans

---
+++ src/beans/Usuarios.java
@@ -0,0 +1,30 @@
+package beans;
+
+/**
+ * @CLASSE Destinada ao transporte de dados ref. aos usuários
+ *
+ * @author Ruberval Brasileiro
+ * @version 1.0 iniciada em 26/10/2024
+ */
+public class Usuarios {
+
+    private String usuario;
+    private String senha;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
